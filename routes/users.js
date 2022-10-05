62cd316fc5dcf42ac8b1f3a8f5d0ab4b7de4162f
Stephen <kilgannon.stephen1@gmail.com>
2022-10-05 21:22:07 +0100

    Adding Mongo schemas

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../util/models/user');
+const User = require('../util/models/User');
 
 
 /* GET users listing. */
